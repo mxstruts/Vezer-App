b4d1605c84812dc6749c1d52889d776564c2dbdd
mxstruts <shatilo.max@gmail.com>
2024-03-14 01:55:10 -0400

    Added Mapbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+'use client'
 import AirPollution from './Components/AirPollution/AirPollution'
 import DailyForecast from './Components/DailyForecast/DailyForecast'
 import FeelsLike from './Components/FeelsLike/FeelsLike'
 import Humidity from './Components/Humidity/Humidity'
+import Mapbox from './Components/Mapbox/Mapbox'
 import Navbar from './Components/Navbar'
 import Population from './Components/Population/Population'
 import Pressure from './Components/Pressure/Pressure'
@@ -10,8 +12,21 @@ import Temperature from './Components/Temperature/Temperature'
 import UvIndex from './Components/UvIndex/UvIndex'
 import Visibility from './Components/Visibility/Visibility'
 import Wind from './Components/Wind/Wind'
+import defaultStates from './utils/defaultStates'
+import { useGlobalContextUpdate } from './context/globalContext'
 
 export default function Home() {
+	const { setActiveCityCoords } = useGlobalContextUpdate()
+
+	const getClickedCityCords = (lat: number, lon: number) => {
+		setActiveCityCoords([lat, lon])
+
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		})
+	}
+
 	return (
 		<main className='mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto'>
 			<Navbar />
@@ -31,6 +46,29 @@ export default function Home() {
 						<Humidity />
 						<Visibility />
 						<Pressure />
+					</div>
+					<div className='mapbox-con mt-4 flex gap-4'>
+						<Mapbox />
+						<div className='states flex flex-col gap-3 flex-1'>
+							<h2 className='flex items-center gap-2 font-medium'>
+								Top Large Cities
+							</h2>
+							<div className='flex flex-col gap-4'>
+								{defaultStates.map((state, index) => {
+									return (
+										<div
+											key={index}
+											className='border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none'
+											onClick={() => {
+												getClickedCityCords(state.lat, state.lon)
+											}}
+										>
+											<p className='px-6 py-4'>{state.name}</p>
+										</div>
+									)
+								})}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
